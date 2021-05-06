c737b678fadb8edc9ef2b3c59fe59428c9f04b6e
zhenghaoxu <tidus-zheng@hotmail.com>
2021-05-06 23:39:25 +0900

    fix: add dependence

---
+++ RayTracingTest/RayTracingTest.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+// error call back
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main()
+{
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        printf("init");
+    }
+
+
+}
+
