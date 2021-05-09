3e1b75fc5eb2de5c84786b2bcd39a01d18beca80
Tidus-Zheng <65854395+Tidus-Zheng@users.noreply.github.com>
2021-05-09 22:57:49 +0900

    printf shader error log

--- RayTracingTest/RayTracingTest.cpp
+++ RayTracingTest/RayTracingTest.cpp
@@ -7,16 +7,17 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
 
 static const struct
 {
-    float x, y;
-    float r, g, b;
+	float x, y;
+	float r, g, b;
 } vertices[3] =
 {
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+	{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
+	{  0.6f, -0.4f, 0.f, 1.f, 0.f },
+	{   0.f,  0.6f, 0.f, 0.f, 1.f }
 };
 
 static const char* vertex_shader_text =
@@ -41,115 +42,136 @@ static const char* fragment_shader_text =
 
 static void error_callback(int error, const char* description)
 {
-    fprintf(stderr, "Error: %s\n", description);
+	fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 int main(void)
 {
-    GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+	GLFWwindow* window;
+	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+	GLint mvp_location, vpos_location, vcol_location;
 
-    glfwSetErrorCallback(error_callback);
+	glfwSetErrorCallback(error_callback);
 
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
 
-    glfwSetKeyCallback(window, key_callback);
+	glfwSetKeyCallback(window, key_callback);
 
-    glfwMakeContextCurrent(window);
-    gladLoadGL(glfwGetProcAddress);
-    glfwSwapInterval(0); //1: 60fps, 0:unlimit
+	glfwMakeContextCurrent(window);
+	gladLoadGL(glfwGetProcAddress);
+	glfwSwapInterval(0); //1: 60fps, 0:unlimit
 
-    // NOTE: OpenGL error checks have been omitted for brevity
+	// NOTE: OpenGL error checks have been omitted for brevity
 
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+	glCompileShader(vertex_shader);
 
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
+	GLint result;
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		GLint length;
+		glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &length);
+		std::vector<char> error(length);
+		glGetShaderInfoLog(vertex_shader, length, &length, &error[0]);
+		std::cout << "Failed to compile vertex shader: " << &error[0] << std::endl;
+		glDeleteShader(vertex_shader);
+	}
 
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+	glCompileShader(fragment_shader);
 
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		GLint length;
+		glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &length);
+		std::vector<char> error(length);
+		glGetShaderInfoLog(fragment_shader, length, &length, &error[0]);
+		std::cout << "Failed to compile fragment shader: " << &error[0] << std::endl;
+		glDeleteShader(fragment_shader);
+	}
 
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-        sizeof(vertices[0]), (void*)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+	program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
 
-    float currentTime = static_cast<float>(glfwGetTime());
-    float lastFramesPrint = currentTime;
-    float frameCount = 0;
+	mvp_location = glGetUniformLocation(program, "MVP");
+	vpos_location = glGetAttribLocation(program, "vPos");
+	vcol_location = glGetAttribLocation(program, "vCol");
 
-    while (!glfwWindowShouldClose(window))
-    {
-        //check fps
-        frameCount++;
-        float newTime = static_cast<float>(glfwGetTime());
-        float deltaTime = newTime - currentTime;
-        currentTime = newTime;
+	glEnableVertexAttribArray(vpos_location);
+	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+		sizeof(vertices[0]), (void*)0);
+	glEnableVertexAttribArray(vcol_location);
+	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+		sizeof(vertices[0]), (void*)(sizeof(float) * 2));
 
-        if (newTime - lastFramesPrint > 1.0f) {
-            std::cout << "FPS: " << frameCount << std::endl;
-            lastFramesPrint = newTime;
-            frameCount = 0;
-        }
+	float currentTime = static_cast<float>(glfwGetTime());
+	float lastFramesPrint = currentTime;
+	float frameCount = 0;
 
-        float ratio;
-        int width, height;
-        mat4x4 m, p, mvp;
+	while (!glfwWindowShouldClose(window))
+	{
+		//check fps
+		frameCount++;
+		float newTime = static_cast<float>(glfwGetTime());
+		float deltaTime = newTime - currentTime;
+		currentTime = newTime;
 
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float)height;
+		if (newTime - lastFramesPrint > 1.0f) {
+			std::cout << "FPS: " << frameCount << std::endl;
+			lastFramesPrint = newTime;
+			frameCount = 0;
+		}
 
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
+		float ratio;
+		int width, height;
+		mat4x4 m, p, mvp;
 
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
 
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+		mat4x4_identity(m);
+		mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+		mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		mat4x4_mul(mvp, p, m);
 
-    glfwDestroyWindow(window);
+		glUseProgram(program);
+		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
